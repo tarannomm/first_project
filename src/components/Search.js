1347a176b5a8f0253b706e70b73f009c4ada7f8f
Tarannom Azimi <tarannom.azimi99@gmail.com>
2023-02-28 22:19:19 +0330

    set search input content

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../styles/scss/Search.scss";
 const Search = () => {
+    const [search,setSearch]=useState('');
+   
     return (
         <div className='searchContainer'>
             <h1>Search what you want !!!</h1>
-            <input type="search" placeholder='search...'/>
+            <input type="search" placeholder='search...' value={search}  onChange={e => setSearch(e.target.value)}/>
         </div>
     );
 };
