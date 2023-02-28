497f4774412894a91ea56e0a54f2db28aaeb167e
Tarannom Azimi <tarannom.azimi99@gmail.com>
2023-02-28 21:53:48 +0330

    create a hamburger menu for responsive states

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../styles/css/Navbar.css"
 import logo from "../images/js.png";
 const Navbar = () => {
+    const[show,setShow]=useState(false);
     return (
-        <div className='navContainer'>
-            <ul className='navList'>
+        <div className='navContainer'> 
+            <img src={logo} alt='logo'/>
+            <ul className={show?'navList2':'navList'}>
                 <li><a href='#'>Home</a></li>
                 <li><a href='#'>Products</a></li>
                 <li><a href='#'>About us</a></li>
             </ul>
-            <img src={logo} />
+            <div className={show?'hamburgerMenu2':'hamburgerMenu'} onClick={()=>setShow(!show)}>
+                <span id='s1'></span>
+                <span id='s2'></span>
+                <span id='s3'></span>
+            </div>
         </div>
     );
 };
-
 export default Navbar;
