94ea856304dd3ab062d155255a3828cd966ac28b
Tarannom Azimi <tarannom.azimi99@gmail.com>
2023-02-28 22:44:51 +0330

    create counter part in card componet

--- src/components/shared/Card.js
+++ src/components/shared/Card.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/css/Card.css";
+import up from "../../images/up.svg";
+import down from "../../images/down.svg";
 const Card = (props) => {
   const { image, title, price } = props;
+  const [count,setCount]=useState(0);
+  const downCounter=()=>{
+    if(count>0){
+    setCount(count-1)
+    }
+
+  }
   return (
     <div className="cardContainer">
       <img src={image} alt="products" />
       <div className="contentContainer">
         <h3>{title}</h3>
         <p>{price}</p>
+      </div>
+      <div className="counter">
+       <img src={up} alt="up" onClick={()=>setCount(count+1)}/>
+       <span className={count?'':'countSpan'}>{count}</span>
+       <img src={down} alt="down" onClick={downCounter}/>
       </div>
     </div>
   );
