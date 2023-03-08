54b6b5b65c5151fba8bb4d3b14df6aa499e0d7ac
Tarannom Azimi <tarannom.azimi99@gmail.com>
2023-03-08 23:37:22 +0330

    search part is responsived

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,14 +8,14 @@ import "../styles/css/Cards.css";
 
 const Cards = () => {
     const [datas,setDatas]=useState([
-        {image:s21 , title:"s21" , price:"800"},
-        {image:iphonex, title:"iphonex" , price:"600"},
-        {image:iphone12, title:"iphone12" , price:"900"},
-        {image:iphone13, title:"iphone13" , price:"1200"}
+        {id:1,image:s21 , title:"s21" , price:"800"},
+        {id:2,image:iphonex, title:"iphonex" , price:"600"},
+        {id:3,image:iphone12, title:"iphone12" , price:"900"},
+        {id:4,image:iphone13, title:"iphone13" , price:"1200"}
     ])
     return (
         <div className='cardsContainer'>
-          {datas.map(product=><Card image={product.image} title={product.title} price={product.price}/>)}
+          {datas.map(product=><Card key={product.id} image={product.image} title={product.title} price={product.price}/>)}
         </div>
     );
 };
