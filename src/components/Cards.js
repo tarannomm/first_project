b51ae26d5907bba22fd6ceb26972b7deb474d324
Tarannom Azimi <tarannom.azimi99@gmail.com>
2023-03-08 22:53:24 +0330

    use list rendering when call Card component in Cards component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './shared/Card';
 import s21 from "../images/s21.jpg";
 import iphonex from "../images/iphonex.jfif";
@@ -7,12 +7,15 @@ import iphone13 from "../images/iphone13.jfif";
 import "../styles/css/Cards.css";
 
 const Cards = () => {
+    const [datas,setDatas]=useState([
+        {image:s21 , title:"s21" , price:"800"},
+        {image:iphonex, title:"iphonex" , price:"600"},
+        {image:iphone12, title:"iphone12" , price:"900"},
+        {image:iphone13, title:"iphone13" , price:"1200"}
+    ])
     return (
         <div className='cardsContainer'>
-            <Card image={s21} title="s21" price="800"/>
-            <Card image={iphonex} title="iphonex" price="600"/>
-            <Card image={iphone12} title="iphone12" price="900"/>
-            <Card image={iphone13} title="iphone13" price="1200"/>
+          {datas.map(product=><Card image={product.image} title={product.title} price={product.price}/>)}
         </div>
     );
 };
