ff080f5b7c75608d5437a8449a4faf0f513b1e35
Tarannom Azimi <tarannom.azimi99@gmail.com>
2023-03-11 19:06:59 +0330

    products part is added

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,25 +1,22 @@
 
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from './shared/Card';
 import axios from 'axios';
-class Products extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            products:[]
-        }
-    }
-    componentDidMount(){
-        axios.get("https://fakestoreapi.com/products")
-        .then(response=>);
-      
-    }
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
-}
+import loading from "../images/loading.gif";
+import '../styles/scss/Products.scss';
+const Products = () => {
+    const [products,setProducts]=useState([]);
+    useEffect(() => {
+         axios.get('https://fakestoreapi.com/products')
+         .then(response=>setProducts(response.data))
+      });
+    return (
+        <div className='productsContrainer'>
+             {products.length? products.map((product)=><Card key={product.id} style={{width:40}} image={product.image} title={product.title} price={product.price}/>):<img style={{margin:100}} src={loading} alt='loading gif'/>}
+            
+        </div>
+    );
+};
 
 export default Products;
+  
